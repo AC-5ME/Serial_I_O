2544282a962e995b184229727871f904863127e1
AC-5ME <99979525+AC-5ME@users.noreply.github.com>
2022-02-24 15:25:13 -0700

    Add files via upload

--- Serial_I_O_Latest.ino
+++ Serial_I_O_Latest.ino
@@ -11,6 +11,12 @@ enum FSM_STATES {     //Byte parser
   FSM_CRC_CHECK,
 };
 
+uint8_t controller_Id;      //Bytes received
+uint8_t packet_Id;
+uint8_t msgByte_high;
+uint8_t msgByte_low;
+uint8_t crcVal_received;
+
 enum SERIAL_REQUEST {     //Display data
   VOLTAGE,
   CURRENT,
@@ -22,19 +28,6 @@ enum SERIAL_REQUEST {     //Display data
   WARNINGS,
   ERRORS,
 };
-
-enum DATA_FLAGS {
-  NO_INFORMATION,
-  DATA_GOOD,
-};
-
-uint8_t magic_Byte;     //Incoming bytes
-uint8_t controller_Id;
-uint8_t packet_Id;
-uint8_t msgByte_low;
-uint8_t msgByte_high;
-uint8_t Data_Corrupted;
-uint8_t crcVal_calculated;
 
 uint16_t blinkPeriod = 500;     // Warning delay
 unsigned long time_now = 0;
@@ -49,14 +42,36 @@ uint8_t query7[] = {254, 00, 9, 9};
 uint8_t query8[] = {254, 00, 10, 10};
 uint8_t query9[] = {254, 00, 11, 11};
 
-void Warning_parser(uint16_t Warning_code)  {        //Warning_code values are good
+void CRC_check (uint8_t controller_Id, uint8_t packet_Id, uint8_t msgByte_high, uint8_t msgByte_low, uint8_t crcVal_received) {     //CRC checker
+
+  uint8_t  crcVal_calculated = (((controller_Id ^ packet_Id) ^ msgByte_high) ^ msgByte_low);
+
+  if (crcVal_received == crcVal_calculated) {
+    uint16_t DataVal;
+    DataVal = (msgByte_low) + (msgByte_high * 256);
+    DisplayData(packet_Id, DataVal);      //Run data display loop
+
+  } else {      //CRC failed
+    int Data_Corrupted;
+    Data_Corrupted ++;
+
+    if (Data_Corrupted < 100) {
+      lcd.setCursor(16, 3);
+      lcd.print ("   ");
+      lcd.setCursor(16, 3);
+      lcd.print(Data_Corrupted);
+    }
+  }
+}
+
+void Warning_parser(uint16_t Warning_code)  {        //Warning_code parser
 
   uint32_t time_for_the_next_loop = millis();
   uint32_t time_for_the_next_message = millis();
 
   switch (Warning_code) {
 
-    case 0x01:
+    case 0x0001:
       time_now = millis();
 
       if (time_now > time_for_the_next_loop) {
@@ -77,7 +92,7 @@ void Warning_parser(uint16_t Warning_code)  {        //Warning_code values are g
       lcd.print("Volts:");
       break;
 
-    case 0x02:
+    case 0x0002:
       time_now = millis();
 
       if (time_now > time_for_the_next_loop) {
@@ -99,7 +114,7 @@ void Warning_parser(uint16_t Warning_code)  {        //Warning_code values are g
       lcd.print("Amps:");
       break;
 
-    case 0x04:
+    case 0x0004:
       time_now = millis();
 
       if (time_now > time_for_the_next_loop) {
@@ -117,7 +132,7 @@ void Warning_parser(uint16_t Warning_code)  {        //Warning_code values are g
       lcd.print("ESC:");
       break;
 
-    case 0x10:
+    case 0x0010:
       time_now = millis();
 
       if (time_now > time_for_the_next_loop) {
@@ -138,46 +153,45 @@ void Warning_parser(uint16_t Warning_code)  {        //Warning_code values are g
   }
 }
 
-void Error_parser(uint16_t Error_code) {        //Error_code vales are good
+void Error_parser(uint16_t Error_code) {        //Error_code parser
 
   switch (Error_code) {
-
-    case 0x02:
+    case 0x0002:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
       lcd.print("NEUTRAL?");
       break;
 
-    case 0x08:
+    case 0x0008:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
       lcd.print("MEMORY FAIL!");
       break;
 
-    case 0x20:
+    case 0x0020:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
       lcd.print("HAll SENSOR!");
       break;
 
-    case 0x40:
+    case 0x0040:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
       lcd.print("12V BUS!");
       break;
 
-    case 0x80:
+    case 0x0080:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
       lcd.print("HALL RESET");
       break;
 
-    case 0x0200:      //Just like datasheet
+    case 0x0200:
       lcd.setCursor (0, 3);
       lcd.print("               ");
       lcd.setCursor (0, 3);
@@ -185,59 +199,49 @@ void Error_parser(uint16_t Error_code) {        //Error_code vales are good
   }
 }
 
-void DisplayData (uint16_t DataVal) {     //Send data to display
+void DisplayData (uint8_t packet_Id, uint16_t DataVal) {     //Display data
 
   switch (packet_Id) {
-    case 0x01:
-
-      //Print Volts
-      lcd.setCursor(6, 1);
-      lcd.print("    ");
-      lcd.setCursor(7, 1);
-      lcd.print(DataVal);
+    case 0x01:       //Print Volts - only gets this far
+      float voltVal = (DataVal / 10);
+      lcd.setCursor(2, 1);
+      lcd.print("        ");
+      lcd.setCursor(3, 1);
+      lcd.print(voltVal);
       break;
 
-    case 0x02:
-
-      //Print Current
-      lcd.setCursor(15, 0);
-      lcd.print("   ");
-      lcd.setCursor(15, 0);
-      lcd.print(DataVal);
+    case 0x02:       //Print Current
+      float currentVal = (DataVal / 10);
+      lcd.setCursor(13, 1);
+      lcd.print("     ");
+      lcd.setCursor(14, 1);
+      lcd.print(currentVal);
       break;
 
-    case 0x03:
-
-      //Print RPM
+    case 0x03:       //Print RPM
       uint16_t rpmValue;
       rpmValue = (DataVal * 10);
       lcd.setCursor(4, 0);
-      lcd.print("   ");
-      lcd.setCursor(4, 0);
+      lcd.print("     ");
+      lcd.setCursor(5, 0);
       lcd.print(rpmValue);
       break;
 
-    case 0x04:
-
-      //Print ESC temp
+    case 0x04:       //Print ESC temp
       lcd.setCursor(14, 2);
       lcd.print("   ");
       lcd.setCursor(14, 2);
       lcd.print(DataVal);
       break;
 
-    case 0x05:
-
-      //Print Motor temp
+    case 0x05:      //Print Motor temp
       lcd.setCursor(6, 2);
       lcd.print("   ");
       lcd.setCursor(6, 2);
       lcd.print(DataVal);
       break;
 
-    case 0x08:
-
-      //Print Power Requested
+    case 0x08:      //Print Power Requested
       const int min = 0, max = 1023;
       const int span = max - min, hundred = 100.0;
       uint16_t PwrReq_Val;
@@ -264,35 +268,12 @@ void DisplayData (uint16_t DataVal) {     //Send data to display
   }
 }
 
-void CRC_check (uint8_t crcVal_received) {     //CRC check, pass byte c = crcVal_received
-
-  crcVal_calculated = (((controller_Id ^ packet_Id) ^ msgByte_high) ^ msgByte_low);
-
-  if (crcVal_received == crcVal_calculated) {
-    uint16_t DataVal;
-    DataVal = (msgByte_low) + (msgByte_high * 256);
-    DisplayData(DataVal);      //Run data display loop
-
-  } else {      //CRC failed
-
-    Data_Corrupted ++;
-
-    if (Data_Corrupted < 100) {
-      lcd.setCursor(16, 3);
-      lcd.print ("   ");
-      lcd.setCursor(16, 3);
-      lcd.print(Data_Corrupted);
-    }
-  }
-}
-
 void ProcessInput (uint8_t c) {      // Response parser
 
   static FSM_STATES state = FSM_MAGIC_BYTE;
 
   switch (state) {
     case FSM_MAGIC_BYTE:       // Check if the first byte is the magic byte
-
       if (c == 0xfe) {
         state = FSM_CONTROLLER_ID;
       }
@@ -326,14 +307,9 @@ void ProcessInput (uint8_t c) {      // Response parser
       break;
 
     case FSM_CRC_CHECK:
-      uint8_t crcVal_received = c;
-
-      Serial.print ("Byte recieved: ");
-      Serial.print (c, HEX);
-      Serial.print (" ");
-      CRC_check(c);     //Run CRC checker
-
-      FSM_STATES state = FSM_MAGIC_BYTE;
+      crcVal_received = c;
+      CRC_check (controller_Id, packet_Id, msgByte_high, msgByte_low, crcVal_received);     //Run CRC checker
+      state = FSM_MAGIC_BYTE;
       break;
   }
 }
@@ -345,35 +321,25 @@ void setup() {
   Serial.begin(2400);
 
   lcd.setCursor(0, 0);      // Set the cursor on the X column and Y row.
-  lcd.print ("RPM:-");
+  lcd.print ("RPM:---");
   lcd.setCursor(0, 1);
-  lcd.print ("Volts:-");
+  lcd.print ("V:---");
   lcd.setCursor(0, 2);
-  lcd.print ("Motor:-");
-  lcd.setCursor(10, 0);
-  lcd.print ("Amps:-");
-  lcd.setCursor(10, 1);
-  lcd.print ("PwrReq:-");
-  lcd.setCursor(10, 2);
-  lcd.print ("ESC:-");
-  lcd.setCursor(0, 3);
-  lcd.print ("NO DATA");
+  lcd.print ("Motor:---");
+  lcd.setCursor(11, 0);
+  lcd.print ("Req%:---");
+  lcd.setCursor(11, 1);
+  lcd.print ("A:---");
+  lcd.setCursor(11, 2);
+  lcd.print ("ESC:---");
 }
 
 void loop () {         //Main loop
 
   //Request loop goes here
 
-  lcd.setCursor(0, 3);
-  lcd.print ("NO DATA");
-
-  if (Serial.available() > 0) {   //Check to see if anything is available in the serial receive buffer
-
+  if (Serial.available() > 0) {
     uint8_t c = Serial.read ();
-
-    // lcd.setCursor(0, 3);      // Set timeout clear display warning field
-    //  lcd.print("           ");
-
     ProcessInput (c);
   }
 }
